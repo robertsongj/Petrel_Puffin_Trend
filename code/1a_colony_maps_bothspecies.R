c845dbd3cca9bf48beaec92df8a35cf8236c2b65
Gutowsky <Sarah.Gutowsky@ec.gc.ca>
2024-04-29 15:49:41 -0300

    map of both species together

--- code/1a_colony_maps_bothspecies.R
+++ code/1a_colony_maps_bothspecies.R
@@ -1,165 +1,165 @@
 
-# Load/install packages and set graphical themes / working directory
 
-my_packs = c('tidyverse',
-             'readxl',
-             'RColorBrewer',
-             'viridis',
-             'jagsUI',
-             'mgcv',
-             'ggrepel',
-             'scales',
-             'sf',
-             'ggspatial')
+# map modelled LESP and ATPU col.lesponies together
 
-if (any(!my_packs %in% installed.packages()[, 'Package'])) {install.packages(my_packs[which(!my_packs %in% installed.packages()[, 'Package'])],dependencies = TRUE)}
-lapply(my_packs, require, character.only = TRUE)
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+library(sf)
+library(sp)
+library(ggspatial) # for scalebars on ggplot maps
+library(ggsflabel)
+library(patchwork)
+library(ggrepel)
+library(mapview)
+library(viridis)
 
-rm(list=ls())
-
-theme_set(theme_bw())
-
-
-# prep map LESP ----
-# Load colony coordinates - prepare to create map
-
-AEA_proj <- "+proj=aea +lat_1=50 +lat_2=70 +lat_0=40 +lon_0=-60 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs "
-
-coords = read_xlsx("data/LESP_colony_coordinates.xlsx", sheet = 1) %>%
-  subset(!is.na(Latitude)&!is.na(Longitude)) %>%
-  st_as_sf(coords = c("Longitude", "Latitude"),crs=4326, remove = FALSE) %>%
-  subset(Country %in% c("Canada")) %>%
-  st_transform(AEA_proj) %>%
-  dplyr::rename(Count = `Estimated no. of mature individuals`)
-
-ggplot(data = coords) + geom_sf()
-
-bbox <- st_bbox(coords) %>% st_as_sfc() %>% st_buffer(10000)
-
-coords$Count_numeric <- as.numeric(coords$Count)
-
-
-# PLOT COLONY LOCATIONS
-
+# bring in ATPU ----
+col.atpu <- read.csv("input/ATPU_recent.counts.coords.csv")
 
 sf_use_s2(FALSE)
 
+col.atpu <- st_as_sf(col.atpu,coords = c("lon", "lat"),crs=4326) 
+mapview(col.atpu)
+hist(col.atpu$recent_count, breaks=50)
+col.atpu <- col.atpu %>% mutate(count_f = case_when(
+  recent_count %in% 0:1000 ~ "<1,000",
+  recent_count %in% 1000:5000 ~ "1,000 to 5,000",
+  recent_count %in% 5000:10000 ~ "5,000 to 10,000",
+  recent_count %in% 10000:50000 ~ "10,000 to 50,000",
+  recent_count %in% 50000:100000 ~ "50,000 to 100,000",
+  recent_count %in% 100000:500000 ~ "100,000 to 500,000",
+  recent_count %in% 500000:1000000 ~ "500,000 to 1,000,000",
+  recent_count > 1000000 ~ "> 1,000,000"))
+summary(as.factor(col.atpu$count_f))
+levels(as.factor(col.atpu$count_f))
+# make it an ordered factor
+col.atpu$count_f <- factor(col.atpu$count_f, levels=c("<1,000",
+                                                      "1,000 to 5,000",
+                                                      "5,000 to 10,000",
+                                                      "10,000 to 50,000",
+                                                      "50,000 to 100,000",
+                                                      "100,000 to 500,000",
+                                                      "500,000 to 1,000,000",
+                                                      "> 1,000,000"))
+summary(col.atpu$count_f)
+
+# bring in LESP ----
+col.lesp <- read.csv("input/LESP_recent.counts.coords.csv")
+
+col.lesp <- st_as_sf(col.lesp,coords = c("lon", "lat"),crs=4326) 
+mapview(col.lesp)
+
+col.lesp <- col.lesp %>% mutate(count_f = case_when(
+  recent_count %in% 0:1000 ~ "<1,000",
+  recent_count %in% 1000:5000 ~ "1,000 to 5,000",
+  recent_count %in% 5000:10000 ~ "5,000 to 10,000",
+  recent_count %in% 10000:50000 ~ "10,000 to 50,000",
+  recent_count %in% 50000:100000 ~ "50,000 to 100,000",
+  recent_count %in% 100000:500000 ~ "100,000 to 500,000",
+  recent_count %in% 500000:1000000 ~ "500,000 to 1,000,000",
+  recent_count > 1000000 ~ "> 1,000,000"))
+summary(as.factor(col.lesp$count_f))
+
+col.lesp$count_f <- factor(col.lesp$count_f, levels=c("<1,000",
+                                                      "1,000 to 5,000",
+                                                      "5,000 to 10,000",
+                                                      "10,000 to 50,000",
+                                                      "50,000 to 100,000",
+                                                      "100,000 to 500,000",
+                                                      "500,000 to 1,000,000",
+                                                      "> 1,000,000"))
+summary(col.lesp$count_f)
+
+# basemap ----
+
+bbox <- st_bbox(c(xmin = -68, xmax = -51, ymin = 42.5, ymax = 55), crs = 4326)
+
 # downloaded from https://www.ngdc.noaa.gov/mgg/shorelines/gshhs.html
-coastline <- st_read('data/spatial_data/Shoreline_Coastline_GSHHG/GSHHS_shp/f/GSHHS_f_L1.shp')
-coastline <- coastline %>%
-  st_make_valid() %>%
-  st_intersection(., st_transform(st_buffer(bbox,100000) , st_crs(.))) %>%
-  st_transform(AEA_proj)
+coastline <- st_read('data/spatial_data/Shoreline_Coastline_GSHHG/GSHHS_shp/f/GSHHS_f_L1.shp') %>% st_crop(bbox)
+mapview(coastline)
+
+basemap <- ggplot(data=coastline) + geom_sf(fill="gray95", size=0.01) + 
+  coord_sf(xlim = c(-68, -51), ylim = c(42.5, 55), expand=FALSE) +
+  theme_bw() +
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank(),
+        axis.text.x = element_text(size=12), axis.text.y = element_text(size=12), 
+        plot.margin = margin(0.2,0.2,0.2,0.2, "cm"), 
+        panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.1), 
+        panel.background = element_rect(fill = "aliceblue")) 
+basemap 
+
+basemap <- basemap +
+  annotate(geom="text", x = -63.2, y=45.1, label = "Nova Scotia", size=4, colour="grey30") +
+  annotate(geom="text", x = -66.3, y=46, label = "New Brunswick", size=4, colour="grey30") +
+  annotate(geom="text", x = -56, y=48.5, label = "Newfoundland", size=4, colour="grey30") +
+  coord_sf(xlim = c(-68, -51), ylim = c(42.5, 55), expand=FALSE) 
+basemap
+
+col_pal <- viridis::plasma(8)
+col_pal <- rev(col_pal)
+
+basemap + geom_point(data=col.lesp, aes(geometry=geometry, color=count_f), size=3, stat="sf_coordinates") + 
+  scale_color_manual(values=col_pal)
+
+basemap + geom_point(data=col.atpu, aes(geometry=geometry, color=count_f), size=3, stat="sf_coordinates") + 
+  scale_color_manual(values=col_pal)
+
+# put them together on one map
+
+col.lesp$sp <- "LESP"
+col.atpu$sp <- "ATPU"
+col <- rbind(col.lesp, col.atpu)
+
+basemap + geom_sf(data=col, aes(geometry=geometry, color=count_f, shape=sp), size=5, stroke=1.5) + 
+  scale_color_manual(values=col_pal) + scale_shape_manual(values=c(1,2)) +
+  coord_sf(xlim = c(-68, -51), ylim = c(42.5, 55), expand=FALSE) 
+
+# nah that's too busy, we'll just stack them but keep the same basemap for easy comparison, but use this dataframe so we get the full legend
 
 
-xlim <- range(as.data.frame(st_coordinates(bbox))$X)
-xlim[2] <- xlim[2] + 200000
-ylim <- range(as.data.frame(st_coordinates(bbox))$Y)
-ylim[1] <- ylim[1]-100000
+col_pal <- viridis::plasma(9)
+col_pal <- rev(col_pal)
+col_pal <- col_pal[c(2:9)]
 
-coords$count_levs <- cut(coords$Count_numeric,
-                         breaks = c(-1,1000,10000,100000,1000000,1e+10),
-                         labels = c("< 1,000",
-                                    "1,000 to 10,000",
-                                    "10,000 to 100,000",
-                                    "100,000 to 1,000,000",
-                                    "> 1,000,000"))
+p1 <- basemap + geom_sf(data=subset(col, sp=="LESP"), aes(geometry=geometry, color=count_f), size=5, stroke=1, shape=1) + 
+  scale_color_manual(values=col_pal, drop = FALSE) +
+  coord_sf(xlim = c(-68, -51), ylim = c(42.5, 55), expand=FALSE) +
+  labs(color = "most recent count") +
+  guides(color = guide_legend(override.aes = list(size = 5))) +
+  theme(legend.spacing.y = unit(1, "mm"), legend.direction="vertical",
+        legend.box="vertical",
+        legend.position="none", 
+        legend.box.background = element_rect(color = "black",fill = "white"),
+        legend.box.margin = margin(0.4,0.4,0.4,0.4,"cm"),
+        legend.background = element_rect(color = "white"),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=14))
+p1
 
+p2 <- basemap + geom_sf(data=subset(col, sp=="ATPU"), aes(geometry=geometry, color=count_f), size=5, stroke=1, shape=1) + 
+  scale_color_manual(values=col_pal, drop = FALSE) +
+  coord_sf(xlim = c(-68, -51), ylim = c(42.5, 55), expand=FALSE) +
+  labs(color = "most recent count") +
+  guides(color = guide_legend(override.aes = list(size = 5))) +
+  theme(legend.spacing.y = unit(1, "mm"), legend.direction="vertical",
+        legend.box="vertical",
+        #legend.position=c(0.8,0.17), 
+        legend.box.background = element_rect(color = "black",fill = "white"),
+        legend.box.margin = margin(0.4,0.4,0.4,0.4,"cm"),
+        legend.background = element_rect(color = "white"),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=14))
+p2
 
+p1 <- p1 + annotate("text", label = "A", x=-Inf, y=Inf, vjust=2, hjust=-1, size=8)
+p2 <- p2 + annotate("text", label = "B", x=-Inf, y=Inf, vjust=2, hjust=-1, size=8)
 
-# https://icolorpalette.com/color/pale-blue
-colony_map <- ggplot() + 
-  geom_sf(data = coastline, col = "gray90", fill = "white") + 
-  
-  # Colonies with estimated abundances (plot these in layers, so that small colonies appear on top)
-  geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[5]), aes(size = count_levs,fill= count_levs), col = "black", shape = 21, stroke = 0.2, alpha = 0.9)+
-  #geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[5]), aes(size = count_levs,fill= count_levs), col = "black", shape = 10, stroke = 0.2)+
-  
-  geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[4]), aes(size = count_levs,fill= count_levs), col = "black", shape = 21, stroke = 0.2, alpha = 0.9)+
-  #geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[4]), aes(size = count_levs,fill= count_levs),  col = "black", shape = 10, stroke = 0.2)+
-  
-  geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[3]), aes(size = count_levs,fill= count_levs), col = "black", shape = 21, stroke = 0.2, alpha = 0.9)+
-  #geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[3]), aes(size = count_levs,fill= count_levs), col = "black", shape = 10, stroke = 0.2)+
-  
-  geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[2]), aes(size = count_levs,fill= count_levs), col = "black", shape = 21, stroke = 0.2, alpha = 0.9)+
-  #geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[2]), aes(size = count_levs,fill= count_levs), col = "black", shape = 10, stroke = 0.2)+
-  
-  geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[1]), aes(size = count_levs,fill= count_levs), col = "black", shape = 21, stroke = 0.2, alpha = 0.9)+
-  #geom_sf(data = subset(coords,count_levs == levels(coords$count_levs)[1]), aes(size = count_levs,fill= count_levs), col = "black", shape = 10, stroke = 0.2)+
-  
-  
-  # Colonies with no estimated abundances
-  geom_sf(data = subset(coords, Count == "Present"), col = "black",fill = "gray20", 
-          size = 1, shape = 10, stroke = 0.2)+
-  
-  coord_sf(xlim = xlim, ylim = ylim)+
-  scale_size_manual(name = "Colony Size",
-                    values = c(1,2,3,5,8),
-                    breaks = levels(coords$count_levs)
-                    
-  )+
-  scale_fill_manual(name = "Colony Size",
-                    #values = rev(magma(8)[2:6])
-                    values = RColorBrewer::brewer.pal(5,"YlGnBu"),
-                    breaks = levels(coords$count_levs)
-  )+
-  
-  annotation_scale(style = "ticks")+
-  annotation_north_arrow(which_north = "true",
-                         location = "tr",
-                         pad_x = unit(0.25, "cm"), pad_y = unit(0.25, "cm"),
-                         height = unit(1, "cm"),
-                         width = unit(1, "cm"),
-                         style = north_arrow_fancy_orienteering(text_col = 'gray20',
-                                                                line_col = 'gray20',
-                                                                fill = 'gray80'))+
-  theme(panel.background = element_rect(fill = "#e3fefe",
-                                        colour = "#e3fefe",
-                                        size = 0.5, linetype = "solid"),
-        legend.spacing.y = unit(0, "mm"), 
-        panel.border = element_rect(colour = "black", fill=NA),
-        #aspect.ratio = 0.2, axis.text = element_text(colour = 1, size = 12),
-        legend.background = element_blank(),
-        legend.box.background = element_rect(colour = "black"),
-        legend.position=c(0.98,0.02),
-        legend.justification = c("right","bottom"),
-        legend.title = element_text(size = 10), 
-        legend.text = element_text(size = 6))+
-  xlab("")+ylab("")
+p <- p1|p2
+p
 
+ggsave(filename="output/figures/maps/both.species.colonies.w.trends.png", plot=p, 
+       device="png", dpi=300, units="cm", width=30, height=15)
 
-# Add map labels
-
-map_labels <- rbind(data.frame(Latitude = 48,Longitude = -62,Label = "Gulf of St Lawrence", angle = 30, size = 3),
-                    data.frame(Latitude = 50,Longitude = -51,Label = "North Atlantic\nOcean", angle = 10, size = 4)) %>%
-  st_as_sf(coords = c("Longitude", "Latitude"),crs=4326, remove = FALSE)
-
-for (i in 1:nrow(map_labels)){
-  colony_map <- colony_map +
-    geom_sf_text(data = map_labels[i,], aes(label = Label, angle = angle),
-                 col = "gray30", alpha = 0.3, size = map_labels$size[i])
-  
-}
-
-print(colony_map)
-
-png(paste0("output/figures/maps/LESP_colony_map.png"), width=6, height=5.5, units="in", res=600, type="cairo")
-print(colony_map)
-dev.off()
-
-
-# Read in data, check out relationship between CV and colony size
-
-
-dat = read_xlsx("data/LESP_data_for_analysis.xlsx", sheet = 1) %>%
-  dplyr::rename(Count = `Mature individuals`)
-
-ggplot(data = subset(dat, Count > 0 & !is.na(CV)), aes(x = Count, y = CV))+
-  geom_point(size=3)+
-  scale_y_continuous(trans = "log10", labels = comma, name = "Coefficient of Variation")+
-  scale_x_continuous(trans = "log10", labels = comma)+
-  ggtitle("Relationship between colony abundance and survey error")+
-  theme_bw()
-
-# Reasonably linear relationship between log (CV) and log (count)
-# - will hopefully allow error to be estimated for surveys that are missing that information
+# need to add province borders and labels but decent start
